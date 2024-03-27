10090ca6d103e01cb053dd3b0f832fb9b48648d4
nicholasali <nicholasali.888@gmail.com>
2024-03-27 19:04:49 +0700

    hapus testing dan useless components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,6 @@
-// Copyright (C) 2021, Mindee.
-
-// This program is licensed under the Apache License version 2.
-// See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0.txt> for full license details.
-
 import { Grid, makeStyles, Theme } from "@material-ui/core";
 import { COLORS } from "@mindee/web-elements.assets";
-import { BrowserView, MobileView } from "react-device-detect";
-import MobileViewPage from "./components/MobileView";
-import PageFooter from "./components/PageFooter";
-import PageHeader from "./components/PageHeader";
 import VisionWrapper from "./components/VisionWrapper";
-import WelcomeMessage from "./components/WelcomeMessage";
 
 const useStyles = makeStyles((theme: Theme) => ({
   wrapper: {
@@ -30,27 +20,19 @@ function App() {
   const classes = useStyles();
   return (
     <>
-      <BrowserView>
-        <Grid className={classes.wrapper} container>
-          <Grid item xs={12}>
-            <PageHeader />
-          </Grid>
-          <Grid spacing={1} className={classes.content} item container xs={12}>
-            <Grid item xs={12}>
-              <WelcomeMessage />
-            </Grid>
-            <Grid item xs={12}>
-              <VisionWrapper />
-            </Grid>
-            <Grid item xs={12}>
-              <PageFooter />
-            </Grid>
-          </Grid>
+      <Grid className={classes.wrapper} container>
+        <Grid item xs={12}>
+          <VisionWrapper />
         </Grid>
-      </BrowserView>
-      <MobileView>
+        {/* <Grid item xs={12}>
+            <PageFooter />
+          </Grid> */}
+        {/* </Grid> */}
+      </Grid>
+      {/* </BrowserView> */}
+      {/* <MobileView>
         <MobileViewPage />
-      </MobileView>
+      </MobileView> */}
     </>
   );
 }
